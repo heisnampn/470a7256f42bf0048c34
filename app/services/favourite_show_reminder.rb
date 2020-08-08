a14506b2994cab85f7039a577064bd9fab7550ab
heisnampn <sunrose@progton.com>
2020-08-08 16:00:16 +0530

    mailer fix

--- app/services/favourite_show_reminder.rb
+++ app/services/favourite_show_reminder.rb
@@ -2,9 +2,10 @@ class FavoutiteShowReminder
   def self.remind
     favourite_tv_shows = TvShow.favourite
     favourite_tv_shows.each do |show|
+      deliver_at =  (30.minutes.ago(Time.parse(show.show_time)) - Time.now)/ 3600	
       User.find_in_batches do |users|
         users.each do |user|
-          UserMailer.show_reminder.deliver_later(wait_until: 10.hours.from_now)
+          UserMailer.show_reminder.deliver_later(wait_until: deliver_at.abs.hours.from_now)
         end 	
       end	
     end	
